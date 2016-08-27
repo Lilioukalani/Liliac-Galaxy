b40037e69b7e16977d96d22866e246b246bda6f4
Lilioukalani <h.liliuokalani@gmail.com>
2016-08-26 17:46:40 -0700

    Update activity_picker.py

--- activity_picker.py
+++ activity_picker.py
@@ -1,10 +1,14 @@
-
 # import speical libaries already built in python
-import random
+import random, urllib2
 # list of options to select from
-possible_activities=['a','b','c']
+the_url = 'https://raw.githubusercontent.com/Lilioukalani/Liliac-Galaxy/master/activities.txt'
+list_raw_text= urllib2.urlopen(the_url).read()
+
+#print "DEBUG: " + str(list_raw_text.split())
+
+
+possible_activities= list_raw_text.split()
 the_activity =random.choice (possible_activities)
 
 print "Possible activities are: " + str(possible_activities)
 print "What we are going to do: " + the_activity
- 
