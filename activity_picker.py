169dc18c9dc6135059c025974c02c1addb7842b3
Lilioukalani <h.liliuokalani@gmail.com>
2016-08-26 17:21:31 -0700

    Update activity_picker.py

--- activity_picker.py
+++ activity_picker.py
@@ -1,5 +1,10 @@
-# import speical libaries already built
 
+# import speical libaries already built in python
+import random
 # list of options to select from
-possible_activities=[]
+possible_activities=['a','b','c']
+the_activity =random.choice (possible_activities)
+
+print "Possible activities are: " + str(possible_activities)
+print "What we are going to do: " + the_activity
  
